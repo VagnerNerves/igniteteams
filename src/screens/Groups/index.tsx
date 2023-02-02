fa75681462c431e127aa3185d6913147e1cdeeb2
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-02 10:51:42 -0300

    feat: Crated const groups and FlaList.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,26 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Hightlight } from '@components/Highlight'
+
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+
   return (
     <Container>
       <Header />
 
       <Hightlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   )
 }
