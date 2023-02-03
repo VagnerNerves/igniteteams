34c0081c0deb6a2338a1e30d4bf13ebe3b11b112
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-03 18:31:22 -0300

    feat: Created Component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import { FlatList } from 'react-native'
 
-import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Hightlight } from '@components/Highlight'
+import { GroupCard } from '@components/GroupCard'
+import { ListEmpty } from '@components/ListEmpty'
 
 import { Container } from './styles'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+  const [groups, setGroups] = useState<string[]>([])
 
   return (
     <Container>
@@ -20,6 +21,10 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
       />
     </Container>
   )
