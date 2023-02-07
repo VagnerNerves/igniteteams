dbbfcffafeb1fe40e74760c60728aefdecb83a40
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-07 18:09:46 -0300

    feat: Ajusted PlaceHolder this Input.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -17,7 +17,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input />
+        <Input placeholder="Nome da Turma" />
 
         <Button title="Criar" style={{ marginTop: 20 }} />
       </Content>
