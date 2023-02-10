4423c4b46507d8ffff761deec23bda27052bbf3f
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-10 20:56:18 -0300

    feat: Finished navigating between the screens.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigation } from '@react-navigation/native'
+
 import { Button } from '@components/Button'
 import { Header } from '@components/Header'
 import { Hightlight } from '@components/Highlight'
@@ -6,6 +8,12 @@ import { Input } from '@components/Input'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const navigation = useNavigation()
+
+  function handleNew() {
+    navigation.navigate('players', { group: 'Rocket' })
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -19,7 +27,7 @@ export function NewGroup() {
 
         <Input placeholder="Nome da Turma" />
 
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   )
