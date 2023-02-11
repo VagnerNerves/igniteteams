0ee6a76cfc3a0629fc11d7bab791ece7204dab2e
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-10 21:30:08 -0300

    feat: Parameters adjusted between screens

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import { Button } from '@components/Button'
@@ -8,10 +9,12 @@ import { Input } from '@components/Input'
 import { Container, Content, Icon } from './styles'
 
 export function NewGroup() {
+  const [group, setGroup] = useState('')
+
   const navigation = useNavigation()
 
   function handleNew() {
-    navigation.navigate('players', { group: 'Rocket' })
+    navigation.navigate('players', { group })
   }
 
   return (
@@ -25,7 +28,7 @@ export function NewGroup() {
           subtitle="crie a turma para adicionar as pessoas"
         />
 
-        <Input placeholder="Nome da Turma" />
+        <Input placeholder="Nome da Turma" onChangeText={setGroup} />
 
         <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
