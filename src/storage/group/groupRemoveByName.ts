0a1234f912924e0ae6f972239baa87ce5949d721
Vagner Nerves <vagnernervessantos@gmail.com>
2023-02-23 19:29:04 -0300

    feat: Ajusted for remove team.

--- src/storage/group/groupRemoveByName.ts
+++ src/storage/group/groupRemoveByName.ts
@@ -1,6 +1,10 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
-import { GROUP_COLLECTION, PLAYER_COLLECTION } from '@storage/storageConfig'
+import {
+  GROUP_COLLECTION,
+  PLAYER_COLLECTION,
+  TEAM_COLLECTION
+} from '@storage/storageConfig'
 
 import { groupsGetAll } from './groupsGetAll'
 
@@ -11,6 +15,7 @@ export async function groupRemoveByName(groupDeleted: string) {
 
     await AsyncStorage.setItem(GROUP_COLLECTION, JSON.stringify(groups))
     await AsyncStorage.removeItem(`${PLAYER_COLLECTION}-${groupDeleted}`)
+    await AsyncStorage.removeItem(`${TEAM_COLLECTION}-${groupDeleted}`)
   } catch (error) {
     throw error
   }
